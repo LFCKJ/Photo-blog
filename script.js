0affa2a91261ef23084f61ff878198bd695f0942
LFCKJ <k71689@naver.com>
2025-04-09 16:29:39 +0900

    랜덤한 이미지 생성성

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const gridContainer = document.querySelector("#gridContainer");
 
 generateBtn.addEventListener("click", () => {
     const img = document.createElement("img");
-    img.src="https://picsum.photos/200/300?random=1";
+
+    // 매번 다른 이미지를 위해 랜덤 숫자 추가
+    const randomNum = Math.floor(Math.random() * 1000);
+    img.src = `https://picsum.photos/200/300?random=${randomNum}`;
 
     gridContainer.appendChild(img);
 });
