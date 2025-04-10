42c29c2e3d0e33f035c7c53f9cbfe2dd18180a5d
LFCKJ <k71689@naver.com>
2025-04-10 09:35:28 +0900

    이미지 30갸 찼을 때 초기화

--- script.js
+++ script.js
@@ -8,5 +8,13 @@ generateBtn.addEventListener("click", () => {
     const randomNum = Math.floor(Math.random() * 1000);
     img.src = `https://picsum.photos/200/300?random=${randomNum}`;
 
-    gridContainer.appendChild(img);
+    gridContainer.appendChild(img); //자식으로 추가
+
+    //이미지가 30개 이상일 경우 모든 이미지 삭제
+    if(gridContainer.children.length >=30){
+        const confirmed = confirm("30장이 다 찼습니다.\n모든 사진을 지우시겠습니까?");
+        if(confirmed){
+            gridContainer.innerHTML=""; //INnerHTML을 ""로 하여 그리드컨테이너에 아무것도 안 넣음
+        }
+    }
 });
