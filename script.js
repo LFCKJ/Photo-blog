0c345c456b8dfbe7651611dfdd4fe47083e77762
LFCKJ <k71689@naver.com>
2025-04-10 09:46:06 +0900

    취소 버튼 누를때 이미지 추가되는 버그 수정

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const generateBtn = document.querySelector("#generateBtn");
 const gridContainer = document.querySelector("#gridContainer");
 
 generateBtn.addEventListener("click", () => {
+    //이미지가 30개 이상일 경우 모든 이미지 삭제
+    if(gridContainer.children.length >=30){
+        const confirmed = confirm("30장이 다 찼습니다.\n모든 사진을 지우시겠습니까?");
+        if(confirmed){
+            gridContainer.innerHTML=""; //INnerHTML을 ""로 하여 그리드컨테이너에 아무것도 안 넣음
+        }else{
+            return;
+        }
+    }
     const img = document.createElement("img");
 
     // 매번 다른 이미지를 위해 랜덤 숫자 추가
@@ -10,11 +19,6 @@ generateBtn.addEventListener("click", () => {
 
     gridContainer.appendChild(img); //자식으로 추가
 
-    //이미지가 30개 이상일 경우 모든 이미지 삭제
-    if(gridContainer.children.length >=30){
-        const confirmed = confirm("30장이 다 찼습니다.\n모든 사진을 지우시겠습니까?");
-        if(confirmed){
-            gridContainer.innerHTML=""; //INnerHTML을 ""로 하여 그리드컨테이너에 아무것도 안 넣음
-        }
-    }
+    
+  
 });
